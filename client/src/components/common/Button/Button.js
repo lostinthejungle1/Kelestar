a662b761893d106cdf88b46adde2235473afb1f7
Liang <lianginchina1999@gmail.com>
2024-02-22 12:56:37 +0800

    add onClick Prop

--- client/src/components/common/Button/Button.js
+++ client/src/components/common/Button/Button.js
@@ -1,9 +1,9 @@
 import React from "react";
 import './Button.css'
 
-function Button({color,content}){
+function Button({color,content,onClick}){
     return (<>
-        <button className={color}>{content}</button>
+        <button className={color} onClick={onClick}>{content}</button>
     </>)
 }
 
