34fe8e958914a4b3e58233f892e5dc132069ebef
Liang <lianginchina1999@gmail.com>
2024-03-04 21:17:13 +0800

    create customer sub routes

---
+++ client/src/routes/CustomerRoutes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Routes,Route} from 'react-router-dom';
+
+
+const CustomerRoutes = ()=>{
+    return (
+        <Routes>
+            <Route path='/dashboard' element={} />
+        </Routes>
+    )
+}
+
+export default CustomerRoutes;
