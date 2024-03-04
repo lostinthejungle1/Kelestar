28ebaa1133f8abd0a2fdf83d11ff6b0b26fc3576
Liang <lianginchina1999@gmail.com>
2024-03-04 21:25:44 +0800

    add vendor tab switching feature

--- client/src/components/VendorTab/VendorTab.js
+++ client/src/components/VendorTab/VendorTab.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./VendorTab.module.css";
+import {useNavigate} from 'react-router-dom';
 
 import { IoIosHome } from "react-icons/io";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
@@ -7,12 +8,12 @@ import { IoPersonCircleOutline } from "react-icons/io5";
 
 // on which page? dashboard,menu,me
 function VendorTab({page}){
-
+    const navigate = useNavigate();
     return (
         <div className={styles.tab}>
-            <IoIosHome className={page==='dashboard'?styles.active:''}/>
-            <MdOutlineRestaurantMenu className={page==='menu'?styles.active:''} />
-            <IoPersonCircleOutline className={page==='me'?styles.active:''} />
+            <IoIosHome className={page==='dashboard'?styles.active:''} onClick={()=>navigate('/vendor/dashboard')}/>
+            <MdOutlineRestaurantMenu className={page==='menu'?styles.active:''} onClick={()=>navigate('/vendor/homemenu')}/>
+            <IoPersonCircleOutline className={page==='me'?styles.active:''} onClick={()=>navigate('/vendor/me')}/>
         </div>
     )
 }
