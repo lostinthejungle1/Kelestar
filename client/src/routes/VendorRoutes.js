f45dbddbdb33c949b40d27e9419bfef008611759
Liang <lianginchina1999@gmail.com>
2024-03-04 21:26:17 +0800

    add home and me to vendor routes

--- client/src/routes/VendorRoutes.js
+++ client/src/routes/VendorRoutes.js
@@ -4,6 +4,8 @@ import RegisterVendor from '../pages/Register/vendor/RegisterVendor/RegisterVend
 import RegisterMenu from '../pages/Register/vendor/RegisterMenu/RegisterMenu';
 import CreateDish from '../pages/Register/vendor/CreateDish/CreateDish';
 import DashBoard from "../pages/Vendor/DashBoard/DashBoard";
+import HomeMenu from "../pages/Vendor/HomeMenu/HomeMenu";
+import Me from "../pages/Vendor/Me/Me";
 
 const VendorRoutes = ()=>{
     return (
@@ -12,6 +14,8 @@ const VendorRoutes = ()=>{
             <Route path='/register-menu' element={<RegisterMenu/>} />
             <Route path='/create-dish' element={<CreateDish/>} />
             <Route path='/dashboard' element={<DashBoard/>} />
+            <Route path='/homemenu' element={<HomeMenu/>} />
+            <Route path='/me' element={<Me/>} />
         </Routes>
     )
 }
