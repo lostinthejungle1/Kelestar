03622737eb99d62a34314b222cb33ee521014978
Liang <lianginchina1999@gmail.com>
2024-03-04 21:18:16 +0800

    create 404 not found page

---
+++ client/src/pages/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './NotFoundPage.js';
+
+function NotFoundPage(){
+    return(
+        <div>
+            <h1>404 Not found</h1>
+        </div>
+    )
+}
+
+export default NotFoundPage;
