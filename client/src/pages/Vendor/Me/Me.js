54705ea66fc3eba224af27755d1755813a3cc652
Liang <lianginchina1999@gmail.com>
2024-03-04 21:16:56 +0800

    create me page

---
+++ client/src/pages/Vendor/Me/Me.js
@@ -0,0 +1,15 @@
+import React from "react";
+import VendorTab from "../../../components/VendorTab/VendorTab";
+import styles from './Me.module.css';
+
+function Me(){
+
+    return (
+        <div>
+            <h1>Me</h1>
+            <VendorTab page='me' />
+        </div>
+    )
+}
+
+export default Me;
