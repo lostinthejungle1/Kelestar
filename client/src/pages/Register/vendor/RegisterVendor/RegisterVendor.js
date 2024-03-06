76c2ca191fd856d0d0de3b6e3b428e1c490f0103
Liang <lianginchina1999@gmail.com>
2024-03-06 15:37:41 +0800

    allow conditionally rendering edit

--- client/src/pages/Register/vendor/RegisterVendor/RegisterVendor.js
+++ client/src/pages/Register/vendor/RegisterVendor/RegisterVendor.js
@@ -4,7 +4,7 @@ import InputText from "../../../../components/common/Input/InputText/InputText";
 import InputTextarea from "../../../../components/common/Input/InputTextarea/InputTextarea";
 import ThumbnailUploader from "../../../../components/common/Input/ThumbnailUploader/ThumbnailUploader";
 import { IoIosArrowBack } from "react-icons/io";
-import {useNavigate} from 'react-router-dom';
+import {useNavigate,useParams} from 'react-router-dom';
 import styles from './RegisterVendor.module.css';
 
 function RegisterVendor(){
@@ -14,14 +14,21 @@ function RegisterVendor(){
         e.preventDefault();
         navigate('/register-role')
     }
+    const {status} = useParams();
+    console.log(status)
     //TODO: Validate form 
     return (<div >
         <IoIosArrowBack className={styles.return_btn} onClick={()=>{
-            navigate('/register-role');
+            if(status==='register'){
+                navigate('/register-role');
+            }else if(status==='edit'){
+                navigate('/vendor/homemenu');
+            }
         }} />
         {/* update classname */}
         <form className={styles.register_vendor_form} onSubmit={handleSubmit} novalidate>
-            <h2 className={styles.instruction}>Let’s create your vendor detail page</h2>
+            <h2 className={styles.instruction}>{status==='register'?"Let’s create your vendor detail page":'Edit vendor detail page'}</h2>
+            {/* TODO: more logics here, loading vendor uploded info if its editing the page*/}
             <InputText placeholder='Enter your vendor name' label='Vendor name:' /> 
             <ThumbnailUploader label="Thumbnail image:" />
             <InputTextarea placeholder='Enter your vendor description' label='Vendor description:' cols='10' />
